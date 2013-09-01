a8ed49a51846aad1375a74f7363a62180c9bebde
Aaron D. Fernandes <aaron.fernandes@gmail.com>
2013-09-01 12:24:13 -0400

    Ensured UTF8 encoding in DB settings

--- django-nonrel/ocl/settings.py
+++ django-nonrel/ocl/settings.py
@@ -17,6 +17,10 @@ DATABASES = {
         'PASSWORD': '',                       # Not used with sqlite3.
         'HOST': '',                           # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                           # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS':
+            {'init_command':
+                  'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'
+            },
     }
 }
 
