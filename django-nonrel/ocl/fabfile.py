ade6da6baf770e9757ea9bd1fc06ab492e41af6b
Aaron D. Fernandes <aaron.fernandes@gmail.com>
2014-07-19 21:58:51 -0400

    Tweaked some deployment steps

--- django-nonrel/ocl/fabfile.py
+++ django-nonrel/ocl/fabfile.py
@@ -9,7 +9,7 @@ __author__ = 'misternando'
 
 BACKUP_DIR = '/var/backups/ocl'
 CHECKOUT_DIR = '/var/tmp'
-DEPLOY_DIR = '/opt/deploy/django'
+DEPLOY_DIR = '/opt/deploy'
 
 env.user = os.environ['FAB_USER']
 env.password = os.environ['FAB_PASSWORD']
@@ -29,9 +29,9 @@ def test_local():
 
 def backup():
     with cd(DEPLOY_DIR):
-        run("tar -czvf ocl_`date +%Y%m%d`.tgz ocl")
+        run("tar -czvf ocl_`date +%Y%m%d`.tgz django solr")
         run("mv ocl_*.tgz %s" % BACKUP_DIR)
-        run("rm -rf ocl")
+        run("rm -rf django solr")
 
 
 def checkout():
@@ -39,12 +39,18 @@ def checkout():
         run("rm -rf oclapi")
         run("git clone https://github.com/OpenConceptLab/oclapi.git")
 
+
 def provision():
     with cd(CHECKOUT_DIR):
-        run("cp -r oclapi/django-nonrel/ocl %s" % DEPLOY_DIR)
-    with cd("%s/ocl" % DEPLOY_DIR):
+        run("cp -r oclapi/django-nonrel %s/django" % DEPLOY_DIR)
+        run("cp -r oclapi/solr %s" % DEPLOY_DIR)
+        sudo("chown -R solr:wheel %s/solr" % DEPLOY_DIR)
+    with cd("%s/django/ocl" % DEPLOY_DIR):
+        run("cp settings.py.deploy settings.py")
         with prefix("source /opt/virtualenvs/ocl/bin/activate"):
             run("pip install -r requirements.txt")
+            run("./manage.py build_solr_schema > /opt/deploy/solr/collection1/conf/schema.xml")
+            run("./manage.py rebuild_index")
 
 
 def restart():
