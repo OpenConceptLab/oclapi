de9f5295a32e203ebdc014bf779d97f19f2285bf
Aaron D. Fernandes <aaron.fernandes@gmail.com>
2014-12-12 23:49:13 -0500

    #60: Honor the includeRetired flag in the Solr search

--- django-nonrel/ocl/concepts/filters.py
+++ django-nonrel/ocl/concepts/filters.py
@@ -8,6 +8,8 @@ class PublicConceptsSearchFilter(SimpleHaystackSearchFilter):
     def get_filters(self, request, view):
         filters = super(PublicConceptsSearchFilter, self).get_filters(request, view)
         filters.update({'public_can_view': True})
+        if not view.include_retired:
+            filters.update({'retired': False})
         return filters
 
 
@@ -16,5 +18,7 @@ class LimitSourceVersionFilter(SimpleHaystackSearchFilter):
     def get_filters(self, request, view):
         filters = super(LimitSourceVersionFilter, self).get_filters(request, view)
         filters.update({'source_version': view.parent_resource_version.id})
+        if not view.include_retired:
+            filters.update({'retired': False})
         return filters
 
