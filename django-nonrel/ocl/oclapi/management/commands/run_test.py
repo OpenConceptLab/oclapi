71ddc308ba6d76f468e63367737017ca86d8a974
Sny <sny.aggarwal@gmail.com>
2016-08-05 09:58:01 +0530

    misc | correcting the author for run_test

--- django-nonrel/ocl/oclapi/management/commands/run_test.py
+++ django-nonrel/ocl/oclapi/management/commands/run_test.py
@@ -2,7 +2,7 @@ from django.core.management import BaseCommand
 from configurations.management import execute_from_command_line
 
 
-__author__ = 'sny'
+__author__ = 'Sny'
 
 
 class Command(BaseCommand):
