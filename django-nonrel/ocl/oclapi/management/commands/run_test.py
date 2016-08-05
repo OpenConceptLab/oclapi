e351047dfcedeeed4517f9ac9d5958d2d8c54315
Sny <sny.aggarwal@gmail.com>
2016-08-05 09:56:17 +0530

    misc | adding run_test command to run oclapi specific test and not django framework specs

---
+++ django-nonrel/ocl/oclapi/management/commands/run_test.py
@@ -0,0 +1,13 @@
+from django.core.management import BaseCommand
+from configurations.management import execute_from_command_line
+
+
+__author__ = 'sny'
+
+
+class Command(BaseCommand):
+    """ Command to import JSON lines mapping file into OCL """
+    help = 'run tests for oclapi'
+
+    def handle(self, *args, **options):
+        execute_from_command_line(['manage.py', 'test', 'users', 'concepts', 'orgs', 'oclapi', 'sources', 'collection', 'mappings'])
