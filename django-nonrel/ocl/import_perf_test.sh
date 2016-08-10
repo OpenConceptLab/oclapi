43eea3a48e0a71e6d369f831fd725cc34557563c
Rohit Garg <rohitgarg19@gmail.com>
2016-08-10 15:53:07 +0530

    Fixing time command issue on perf tests

--- django-nonrel/ocl/import_perf_test.sh
+++ django-nonrel/ocl/import_perf_test.sh
@@ -3,8 +3,12 @@ python manage.py syncdb --noinput
 #create org and source
 OBJECT_ID=`mongo ocl perf_data/prepare_db.js | grep ObjectId`
 SOURCE=`expr substr $OBJECT_ID 11 24`
+start=`date +%s`
 yes | python manage.py rebuild_index
 #first time import
-time python manage.py import_concepts_to_source --source $SOURCE --token PERF_TEST_TOKEN perf_data/ciel_20160711_concepts_2k.json
-yes | time python manage.py rebuild_index
-# time python manage.py import_mappings_to_source --source 572343325162890014a3424b --token PERF_TEST_TOKEN perf_data/ciel_20160711_mappings_2k.json
+python manage.py import_concepts_to_source --source $SOURCE --token PERF_TEST_TOKEN perf_data/ciel_20160711_concepts_2k.json
+yes | python manage.py rebuild_index
+end=`date +%s`
+runtime=$((end-start))
+echo "Took $(runtime) sec to complete concepts"
+# python manage.py import_mappings_to_source --source 572343325162890014a3424b --token PERF_TEST_TOKEN perf_data/ciel_20160711_mappings_2k.json
