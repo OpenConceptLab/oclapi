c5939fe524e8cdbe59923a5c5e2044c6944c4ae3
Sny <sny.aggarwal@gmail.com>
2016-08-22 13:43:31 +0530

    +Bhawna | 178 | changing docker-compose file and bash script to be generic based on docker env variables

--- django-nonrel/ocl/deploy/start_docker.sh
+++ django-nonrel/ocl/deploy/start_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ~/oclapi/django-nonrel/ocl && docker-compose -f docker-compose.$1.yml build ocl ocl_worker solr && docker-compose -f docker-compose.$1.yml up -d
+cd ~/oclapi/django-nonrel/ocl && docker-compose build ocl ocl_worker solr && docker-compose up -d
--- django-nonrel/ocl/startup.sh
+++ django-nonrel/ocl/startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "from django.contrib.auth.models import User; user = User.objects.create_superuser('root', 'root@example.com', 'Root123') if (User.objects.filter(username='root').count() < 1) else None" | python manage.py shell
+echo "Settings for: $1"
+echo "Configurations for: $2"
 
-python manage.py syncdb --noinput --settings='oclapi.settings.local' --configuration='Local'
-
-python manage.py runserver 0.0.0.0:8000 --settings='oclapi.settings.local' --configuration='Local'
+python manage.py syncdb --noinput --settings="oclapi.settings.$1" --configuration="$2"
+python manage.py runserver 0.0.0.0:8000 --settings="oclapi.settings.$1" --configuration="$2"
