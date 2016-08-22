2c88f84ada689fa850d0fd81d215038a7b12951c
Sny <sny.aggarwal@gmail.com>
2016-08-22 14:39:26 +0530

    +Bhawna | 178 | removing showcase specific scripts

--- django-nonrel/ocl/startup_showcase.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "right place"
-echo "from django.contrib.auth.models import User; user = User.objects.create_superuser('root', 'root@example.com', 'Root123') if (User.objects.filter(username='root').count() < 1) else None" | python manage.py shell
-
-python manage.py syncdb --noinput --settings='oclapi.settings.showcase' --configuration='Showcase'
-python manage.py runserver 0.0.0.0:8000 --settings='oclapi.settings.showcase' --configuration='Showcase'
