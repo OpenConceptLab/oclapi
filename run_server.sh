0428d8caefcb5503e12f9da6a43b5ad07d3233cb
Sny <sny.aggarwal@gmail.com>
2016-08-23 11:18:27 +0530

    178 | run backend script with IP externalized

---
+++ run_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+export RELEASE_VERSION=`date +"%Y%m%d%H%M%S"`
+tar -czf oclapi$RELEASE_VERSION.tgz repo
+scp oclapi$RELEASE_VERSION.tgz root@$IP:/root/releases/
+if ssh root@$IP "~/oclapi/django-nonrel/ocl/deploy/kill_docker.sh"; then echo "killed docker processes"; fi;
+ssh root@$IP "rm -rf oclapi"
+ssh root@$IP "tar -xzf releases/oclapi$RELEASE_VERSION.tgz && mv repo oclapi"
+ssh root@$IP "chmod -R 777 oclapi/solr/collection1"
+ssh root@$IP "~/oclapi/django-nonrel/ocl/deploy/start_docker.sh"
+ssh root@$IP "docker ps"
