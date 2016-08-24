c15fa8ab736bf74823a0759239f7cc71dca20682
Sny <sny.aggarwal@gmail.com>
2016-08-24 14:46:42 +0530

    +Anshu | 189 | correcting ciel dict mappings json path

--- django-nonrel/ocl/import_perf_test_full_ciel_with_docker.sh
+++ django-nonrel/ocl/import_perf_test_full_ciel_with_docker.sh
@@ -5,4 +5,4 @@ export DJANGO_CONFIGURATION=Staging
 OBJECT_ID=`mongo ocl perf_data/prepare_bulk_import_db_with_root_user.js | grep ObjectId`
 SOURCE=`expr substr $OBJECT_ID 11 24`
 time docker-compose run ocl python manage.py import_concepts_to_source --source $SOURCE --token $1 --inline-indexing true ~/ciel_20160711/ciel_20160711_concepts.json
-time docker-compose run ocl python manage.py import_mappings_to_source --source $SOURCE --token $1 --inline-indexing true perf_data/ciel_20160711_mappings_2k.json
+time docker-compose run ocl python manage.py import_mappings_to_source --source $SOURCE --token $1 --inline-indexing true ~/ciel_20160711/ciel_20160711_mappings.json
