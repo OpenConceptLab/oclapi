bffb041ff508a1c06b4982d64e698d487e7c13ce
Sny <sny.aggarwal@gmail.com>
2016-08-25 11:35:29 +0530

    +Anshu | 189 | correcting script

--- django-nonrel/ocl/import_perf_test_full_ciel_with_docker.sh
+++ django-nonrel/ocl/import_perf_test_full_ciel_with_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
-SOURCE=`sh ./export_vars_for_import_script`
+
+BULK_IMPORT_SCRIPT="`cat perf_data/prepare_bulk_import_db_with_root_user.js`"
+echo $BULK_IMPORT_SCRIPT
+OBJECT_ID=`docker exec -t ocl_mongo mongo ocl --eval "$BULK_IMPORT_SCRIPT" | grep ObjectId`
+echo $OBJECT_ID
+SOURCE=`echo $OBJECT_ID | cut -c 11-34`
 export DJANGO_CONFIGURATION=Staging
 echo "Importing in Source -- $SOURCE"
 time docker-compose run ocl python manage.py import_concepts_to_source --source $SOURCE --token $1 --inline-indexing true ciel_20160711/ciel_20160711_concepts.json
