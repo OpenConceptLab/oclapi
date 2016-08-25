3f799b449ed77410cd1651a2b1e69076309e0874
Sny <sny.aggarwal@gmail.com>
2016-08-25 11:30:09 +0530

    +Anshu +Rohit | 189 | merging vars script into one | corrections

--- django-nonrel/ocl/import_perf_test_full_ciel_with_docker.sh
+++ django-nonrel/ocl/import_perf_test_full_ciel_with_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
-
-export DJANGO_CONFIGURATION=Staging && source ./export_vars_for_import_script
+SOURCE=`sh ./export_vars_for_import_script`
+export DJANGO_CONFIGURATION=Staging
 echo "Importing in Source -- $SOURCE"
 time docker-compose run ocl python manage.py import_concepts_to_source --source $SOURCE --token $1 --inline-indexing true ciel_20160711/ciel_20160711_concepts.json
 time docker-compose run ocl python manage.py import_mappings_to_source --source $SOURCE --token $1 --inline-indexing true ciel_20160711/ciel_20160711_mappings.json
