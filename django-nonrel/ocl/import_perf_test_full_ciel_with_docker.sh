c5eae18432435b0f01d0d898b8af090d4745963f
Sny <sny.aggarwal@gmail.com>
2016-08-25 09:51:31 +0530

    +Anshu | 189 | source vars for ciel import script

--- django-nonrel/ocl/import_perf_test_full_ciel_with_docker.sh
+++ django-nonrel/ocl/import_perf_test_full_ciel_with_docker.sh
@@ -1,12 +1,6 @@
 #!/bin/bash -e
 
-export DJANGO_CONFIGURATION=Staging
-#create org and source
-export BULK_IMPORT_SCRIPT="`cat perf_data/prepare_bulk_import_db_with_root_user.js`"
-echo $BULK_IMPORT_SCRIPT
-OBJECT_ID=`docker exec -it ocl_mongo mongo ocl --eval $BULK_IMPORT_SCRIPT | grep ObjectId`
-SOURCE=`expr substr $OBJECT_ID 11 24`
-echo $SOURCE
-echo $1
-time docker-compose run ocl python manage.py import_concepts_to_source --source $SOURCE --token $1 --inline-indexing true ~/ciel_20160711/ciel_20160711_concepts.json
-time docker-compose run ocl python manage.py import_mappings_to_source --source $SOURCE --token $1 --inline-indexing true ~/ciel_20160711/ciel_20160711_mappings.json
+export DJANGO_CONFIGURATION=Staging && source ./export_vars_for_import_script
+echo "Importing in Source -- $SOURCE"
+time docker-compose run ocl python manage.py import_concepts_to_source --source $SOURCE --token $1 --inline-indexing true ciel_20160711/ciel_20160711_concepts.json
+time docker-compose run ocl python manage.py import_mappings_to_source --source $SOURCE --token $1 --inline-indexing true ciel_20160711/ciel_20160711_mappings.json
