ed2e5c7653e8101e9b60482abfd6798889552aa7
Sny <sny.aggarwal@gmail.com>
2016-08-26 13:58:34 +0530

    195 | setting celery once url for integration test

--- django-nonrel/ocl/tasks.py
+++ django-nonrel/ocl/tasks.py
@@ -21,7 +21,12 @@ celery = Celery('tasks', backend='redis://redis.openconceptlab.org:6379/0', brok
 celery.config_from_object('django.conf:settings')
 
 logger = get_task_logger('celery.worker')
-celery.conf.ONCE_REDIS_URL = 'redis://redis.openconceptlab.org:6379/0'
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Local')
+if os.environ.get('DJANGO_CONFIGURATION') == 'IntegrationTest':
+    celery.conf.ONCE_REDIS_URL = 'redis://localhost:6379/0'
+else:
+    celery.conf.ONCE_REDIS_URL = 'redis://redis.openconceptlab.org:6379/0'
+
 
 @celery.task(base=QueueOnce)
 def export_source(version_id):
