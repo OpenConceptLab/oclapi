ed1ba1b51975b3802614770be20c63d3cc63b00d
Sny <sny.aggarwal@gmail.com>
2016-10-03 17:01:55 +0530

    228 | fixing mongo backup script

--- django-nonrel/ocl/deploy/mongo_backup.sh
+++ django-nonrel/ocl/deploy/mongo_backup.sh
@@ -1,6 +1,8 @@
+mkdir -p ~/backups
+
 MONGO_IP=`docker inspect --format '{{ .NetworkSettings.Networks.ocl_default.IPAddress }}' ocl_mongo`
 
 rm -rf /tmp/mongodump && mkdir /tmp/mongodump
-docker run -it --rm --link ocl_mongo:mongo --net=ocl_default -v /tmp/mongodump:/tmp mongo bash -c 'mongodump -v --host 172.18.0.4:27017 --db ocl --out=/tmp'
+docker run -it --rm --link ocl_mongo:mongo --net=ocl_default -v /tmp/mongodump:/tmp mongo bash -c "mongodump -v --host `$MONGO_IP`:27017 --db ocl --out=/tmp"
 tar -cvf ~/backups/mongo_`date +"%Y%m%d%H%M%S"` -C /tmp/mongodump *
 rm -rf /tmp/mongodump
