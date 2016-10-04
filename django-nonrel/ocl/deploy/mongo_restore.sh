807dd529052f4a656c84f1142bfd9fa2c403e9eb
Sny <sny.aggarwal@gmail.com>
2016-10-04 16:52:44 +0530

    228 | updated the mongo restore script usage

--- django-nonrel/ocl/deploy/mongo_restore.sh
+++ django-nonrel/ocl/deploy/mongo_restore.sh
@@ -19,4 +19,6 @@ fi
 docker run -it --rm --link ocl_mongo:mongo --net=ocl_default -v $TMP_DIR:/tmp mongo bash -c 'mongorestore --drop -v --host `echo $MONGO_IP`:27017 --db ocl /tmp/'$FILENAME
 rm -rf $TMP_DIR
 
-#usage is ./mongo-restore.sh ~/backups/<filename>.tar
+#usage :
+# for full db -- ./mongo-restore.sh ~/backups/<filename>.tar
+# for a collection -- ./mongo-restore.sh ~/backups/<dir>/<collection>.json
