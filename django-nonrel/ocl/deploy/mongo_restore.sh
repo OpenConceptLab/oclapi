a183f0e390195366c849ad5102cadaeb03cd31d3
bhawnasingla <bhawnas@thoughtworks>
2016-10-05 12:17:13 +0530

    +Gaurav | Fixed mongo database restore script

--- django-nonrel/ocl/deploy/mongo_restore.sh
+++ django-nonrel/ocl/deploy/mongo_restore.sh
@@ -4,11 +4,9 @@ MONGO_IP=`docker inspect --format '{{ .NetworkSettings.Networks.ocl_default.IPAd
 
 TMP_DIR="/tmp/mongorestore/"
 rm -rf $TMP_DIR && mkdir $TMP_DIR
-if [[ $1 =~ \.tar$ ]];
+if [[ $1 =~ \.tar.gz$ ]];
 then
-        #FILENAME=$(echo $1 | sed 's/.*\///')
         FILENAME=ocl/
-        mkdir $TMP_DIR
         echo "Data will be extracted into :"$TMP_DIR
         tar -C $TMP_DIR -xvf $1
 else
@@ -16,7 +14,7 @@ else
         cp $1 $TMP_DIR$FILENAME
 fi
 
-docker run -it --rm --link ocl_mongo:mongo --net=ocl_default -v $TMP_DIR:/tmp mongo bash -c 'mongorestore --drop -v --host `echo $MONGO_IP`:27017 --db ocl /tmp/'$FILENAME
+docker run -it --rm --link ocl_mongo:mongo --net=ocl_default -v $TMP_DIR:/tmp mongo bash -c "mongorestore --drop -v --host `echo $MONGO_IP`:27017 --db ocl /tmp/$FILENAME"
 rm -rf $TMP_DIR
 
 #usage :
