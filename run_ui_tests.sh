dcc6fa156b3e824fd233c0f5826588a52116d6a4
Emrah B <emrahb@gmail.com>
2016-10-12 11:44:15 +0300

    #0 @nopair add all components to docker-compose-dev // remove extraneous databases setting from Local // mirror run_ui_tests from ocl_web (this needs to be linked via git submodule)

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cd ~/ocl_web
-nohup phantomjs --webdriver=9515 > nohup_ui_tests.out  2>&1 &
-sleep 3
-./node_modules/protractor/bin/protractor ./ocl_web/tests/ui_tests/conf.js
+# env=local browser=phantomjs OCL_WEB=~/workspace/ocl_web run_ui_tests.sh
+
+if [ -z $OCL_WEB ]; then OCL_WEB=~/ocl_web; fi
+
+cd $OCL_WEB
+
+echo "Removing selenium before update"
+rm -rf ./node_modules/protractor/node_modules/webdriver-manager/selenium
+
+echo "Updating web driver manager"
+nohup ./node_modules/protractor/bin/webdriver-manager update
+
+./node_modules/protractor/bin/protractor ./ocl_web/tests/ui_tests/conf.js --verbose
