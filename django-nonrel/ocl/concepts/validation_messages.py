cc2a34bf472f8016fdce402d478a04804d0b22c6
Emrah Bayraktaroglu <emrahb@gmail.com>
2016-12-21 14:00:29 +0300

    #245 update some validation messages (#376)

--- django-nonrel/ocl/concepts/validation_messages.py
+++ django-nonrel/ocl/concepts/validation_messages.py
@@ -8,11 +8,11 @@ OPENMRS_MUST_HAVE_EXACTLY_ONE_PREFERRED_NAME = 'A concept may not have more than
 OPENMRS_SHORT_NAME_CANNOT_BE_PREFERRED = 'A short name cannot be marked as locale preferred'
 OPENMRS_AT_LEAST_ONE_FULLY_SPECIFIED_NAME = 'A concept must have at least one fully specified name (across all locales)'
 OPENMRS_PREFERRED_NAME_UNIQUE_PER_SOURCE_LOCALE = 'Concept preferred name must be unique for same source and locale'
-OPENMRS_DESCRIPTION_LOCALE = 'Description locale must be one of the valid choices'
-OPENMRS_NAME_LOCALE = 'Name locale must be one of the valid choices'
-OPENMRS_DESCRIPTION_TYPE = 'Description type must be one of the valid choices'
-OPENMRS_NAME_TYPE = 'Name type must be one of the valid choices'
-OPENMRS_DATATYPE = 'Data type must be valid attribute'
-OPENMRS_CONCEPT_CLASS = 'Concept class must be valid attribute'
+OPENMRS_DESCRIPTION_LOCALE = 'Invalid description locale'
+OPENMRS_NAME_LOCALE = 'Invalid name locale'
+OPENMRS_DESCRIPTION_TYPE = 'Invalid description type'
+OPENMRS_NAME_TYPE = 'Invalid name type'
+OPENMRS_DATATYPE = 'Invalid data type'
+OPENMRS_CONCEPT_CLASS = 'Invalid concept class'
 
 
