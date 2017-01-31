645ad6976306a4e2c847f52f16054a06011e5d8f
Burak Ince <bince@thoughtworks.com>
2017-01-31 10:19:27 +0300

    #418 Add cleaning up oclapi artifacts older than 30 days

---
+++ django-nonrel/ocl/deploy/manage_releases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear_releases() {
+  RELEASES_DIR=$1
+  if [ -z $1 ]; then RELEASES_DIR="~/releases/"; fi;
+
+  find $RELEASES_DIR -type f -name "oclapi*.tgz" -mtime +30 -exec rm {} \;
+}
