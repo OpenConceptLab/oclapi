c44231da3a4d7c3466fec0ec7c676449e587967a
David Triglianos <davetrig@yahoo.com>
2017-08-22 14:29:18 -0400

    Allow underscores in Concept ID (Issue #16)

--- django-nonrel/ocl/mappings/urls.py
+++ django-nonrel/ocl/mappings/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url
 from mappings.views import MappingListView, MappingDetailView, MappingVersionDetailView, MappingVersionsListView, MappingVersionsView
+from oclapi.models import NAMESPACE_PATTERN, CONCEPT_ID_PATTERN
 
 __author__ = 'misternando'
 
 urlpatterns = patterns('',
     url(r'^$', MappingListView.as_view(), name='mapping-list'),
-    url(r'^(?P<mapping>[a-zA-Z0-9\-\.]+)/$', MappingDetailView.as_view(), name='mapping-detail'),
-    url(r'^(?P<mapping>[a-zA-Z0-9\-\.]+)/versions/$', MappingVersionsView.as_view(), name='mapping-version-list'),
-    url(r'^(?P<mapping>[a-zA-Z0-9\-\.]+)/(?P<mapping_version>[a-zA-Z0-9\-\.]+)/$', MappingVersionDetailView.as_view(), name='mappingversion-detail'),
+    url(r'^(?P<mapping>' + NAMESPACE_PATTERN + ')/$', MappingDetailView.as_view(), name='mapping-detail'),
+    url(r'^(?P<mapping>' + NAMESPACE_PATTERN + ')/versions/$', MappingVersionsView.as_view(), name='mapping-version-list'),
+    url(r'^(?P<mapping>' + NAMESPACE_PATTERN + ')/(?P<mapping_version>' + NAMESPACE_PATTERN + ')/$', MappingVersionDetailView.as_view(), name='mappingversion-detail'),
 )
--- django-nonrel/ocl/sources/urls.py
+++ django-nonrel/ocl/sources/urls.py
@@ -1,24 +1,25 @@
 from django.conf.urls import patterns, url, include
 from sources.feeds import SourceFeed
 from sources.views import SourceListView, SourceRetrieveUpdateDestroyView, SourceVersionRetrieveUpdateView, SourceVersionChildListView, SourceVersionListView, SourceVersionRetrieveUpdateDestroyView, SourceExtrasView, SourceExtraRetrieveUpdateDestroyView, SourceVersionExportView
+from oclapi.models import NAMESPACE_PATTERN, CONCEPT_ID_PATTERN
 
 __author__ = 'misternando'
 
 urlpatterns = patterns('',
     url(r'^$', SourceListView.as_view(), name='source-list'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/$', SourceRetrieveUpdateDestroyView.as_view(), name='source-detail'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/atom/$', SourceFeed()),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/versions/$', SourceVersionListView.as_view(), name='sourceversion-list'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/latest/$', SourceVersionRetrieveUpdateView.as_view(), {'is_latest': True}, name='sourceversion-latest-detail'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/concepts/', include('concepts.urls')),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/mappings/', include('mappings.urls')),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/extras/$', SourceExtrasView.as_view(), name='source-extras'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/extras/(?P<extra>[_a-zA-Z0-9\-\.]+)/$', SourceExtraRetrieveUpdateDestroyView.as_view(), name='source-extra'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/$', SourceVersionRetrieveUpdateDestroyView.as_view(), name='sourceversion-detail'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/children/$', SourceVersionChildListView.as_view(), {'list_children': True}, name='sourceversion-child-list'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/export/$', SourceVersionExportView.as_view(), name='sourceversion-export'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/extras/$', SourceExtrasView.as_view(), name='sourceversion-extras'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/extras/(?P<extra>[_a-zA-Z0-9\-\.]+)/$', SourceExtraRetrieveUpdateDestroyView.as_view(), name='sourceversion-extra'),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/mappings/', include('mappings.urls')),
-    url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/concepts/', include('concepts.urls'))
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/$', SourceRetrieveUpdateDestroyView.as_view(), name='source-detail'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/atom/$', SourceFeed()),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/versions/$', SourceVersionListView.as_view(), name='sourceversion-list'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/latest/$', SourceVersionRetrieveUpdateView.as_view(), {'is_latest': True}, name='sourceversion-latest-detail'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/concepts/', include('concepts.urls')),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/mappings/', include('mappings.urls')),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/extras/$', SourceExtrasView.as_view(), name='source-extras'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/extras/(?P<extra>' + CONCEPT_ID_PATTERN + ')/$', SourceExtraRetrieveUpdateDestroyView.as_view(), name='source-extra'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/(?P<version>' + NAMESPACE_PATTERN + ')/$', SourceVersionRetrieveUpdateDestroyView.as_view(), name='sourceversion-detail'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/(?P<version>' + NAMESPACE_PATTERN + ')/children/$', SourceVersionChildListView.as_view(), {'list_children': True}, name='sourceversion-child-list'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/(?P<version>' + NAMESPACE_PATTERN + ')/export/$', SourceVersionExportView.as_view(), name='sourceversion-export'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/(?P<version>' + NAMESPACE_PATTERN + ')/extras/$', SourceExtrasView.as_view(), name='sourceversion-extras'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/(?P<version>' + NAMESPACE_PATTERN + ')/extras/(?P<extra>' + CONCEPT_ID_PATTERN + ')/$', SourceExtraRetrieveUpdateDestroyView.as_view(), name='sourceversion-extra'),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/(?P<version>' + NAMESPACE_PATTERN + ')/mappings/', include('mappings.urls')),
+    url(r'^(?P<source>' + NAMESPACE_PATTERN + ')/(?P<version>' + NAMESPACE_PATTERN + ')/concepts/', include('concepts.urls'))
 )
