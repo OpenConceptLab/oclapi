eaff0a02e78548a40b091b3248d685caeb6f386e
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-08-25 13:46:24 +0200

    Fixing startup order

--- ocl/startup.sh
+++ ocl/startup.sh
@@ -32,14 +32,14 @@ echo "Configurations for: $CONFIG"
 
 python manage.py syncdb --noinput --settings="oclapi.settings.$SETTINGS" --configuration="$CONFIG"
 
-echo "Importing Lookup Values"
-python manage.py import_lookup_values
-
 echo "Setting up superuser"
 
 if [ -z $ROOT_PASSWORD ]; then ROOT_PASSWORD=Root123; fi;
 
 python manage.py create_tokens --password="$ROOT_PASSWORD" --token="$OCL_API_TOKEN" --create
+
+echo "Importing Lookup Values"
+python manage.py import_lookup_values
 
 echo "Starting the server"
 python manage.py runserver 0.0.0.0:8000 --settings="oclapi.settings.$SETTINGS" --configuration="$CONFIG"
