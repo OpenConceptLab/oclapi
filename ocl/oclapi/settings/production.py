ea8e8ec378a5ff0e39bc8012bcf92ddc6368a653
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-08-28 12:43:31 +0200

    Disable debug for production and staging

--- ocl/oclapi/settings/production.py
+++ ocl/oclapi/settings/production.py
@@ -2,3 +2,5 @@ from oclapi.settings.common import *
 
 class Production(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
+    DEBUG = False
+    TEMPLATE_DEBUG = DEBUG
--- ocl/oclapi/settings/staging.py
+++ ocl/oclapi/settings/staging.py
@@ -2,3 +2,5 @@ from oclapi.settings.common import *
 
 class Staging(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
+    DEBUG = False
+    TEMPLATE_DEBUG = DEBUG
