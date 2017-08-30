3d229fda24ce544b11f39cb4fcbed94295b73e81
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-08-30 10:59:48 +0200

    Get rid off newrelic

--- ocl/manage.py
+++ ocl/manage.py
@@ -8,9 +8,4 @@ if __name__ == "__main__":
     os.environ.setdefault('DJANGO_CONFIGURATION', 'Local')
 
     from configurations.management import execute_from_command_line
-
-    if os.getenv("NEW_RELIC_API_KEY") and os.getenv("DJANGO_CONFIGURATION") in ['Production', 'Staging']:
-        import newrelic.agent
-        newrelic.agent.initialize('newrelic-api.ini')
-
     execute_from_command_line(sys.argv)
