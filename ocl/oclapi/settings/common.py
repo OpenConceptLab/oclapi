d963b801e7e563559460dd4d605caf19987d1e81
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-08-30 12:53:16 +0200

    Set allowed hosts

--- ocl/oclapi/settings/common.py
+++ ocl/oclapi/settings/common.py
@@ -29,7 +29,7 @@ class Common(Configuration):
 
     # Hosts/domain names that are valid for this site; required if DEBUG is False
     # See https://docs.djangoproject.com/en/1.3/ref/settings/#allowed-hosts
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'openconceptlab.org', 'openmrs.org']
 
     # Local time zone for this installation. Choices can be found here:
     # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
