3854e6d288226bde633ac8735a01f6088e9a849a
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-08-31 10:47:00 +0200

    Adding missing qa config

---
+++ ocl/oclapi/settings/qa.py
@@ -0,0 +1,6 @@
+from oclapi.settings.common import *
+
+class Qa(Common):
+    INSTALLED_APPS = Common.INSTALLED_APPS
+
+    SECRET_KEY = values.SecretValue(environ_name='SECRET_KEY', environ_prefix='')
