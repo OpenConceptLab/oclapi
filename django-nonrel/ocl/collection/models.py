abfdf143a43256f155745516c21b491cf80009f8
David Triglianos <davetrig@yahoo.com>
2017-09-09 19:00:47 -0400

    Add integration tests for source and collection version_external_id fields Adjust models to fix error
    (cherry picked from commit 6f62761d30583250b65561d7c27cf23eaa37bac4)

--- django-nonrel/ocl/collection/models.py
+++ django-nonrel/ocl/collection/models.py
@@ -405,7 +405,6 @@ class CollectionVersion(ConceptContainerVersionModel):
             created_by=collection.created_by,
             updated_by=collection.updated_by,
             external_id=collection.external_id,
-            version_external_id=version_external_id,
         )
 
 
