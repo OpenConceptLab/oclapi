1c549209088aaabf771fcf08da2d36e4b74f59c8
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2017-10-05 12:13:28 +0200

    OpenConceptLab/DATIM-Metadata-Project#111 'next' response header returns localhost IP instead of resolved domain name

--- ocl/oclapi/settings/production.py
+++ ocl/oclapi/settings/production.py
@@ -4,3 +4,5 @@ class Production(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
 
     SECRET_KEY = values.SecretValue(environ_name='SECRET_KEY', environ_prefix='')
+
+    USE_X_FORWARDED_HOST = True
