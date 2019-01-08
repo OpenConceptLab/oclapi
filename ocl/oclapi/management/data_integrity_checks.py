beb6a8cb841dd90d75e9997e7403181992fe28b1
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2019-01-08 08:48:12 +0100

    OpenConceptLab/ocl_issues#73 Collection exports contain multiple copies of a resource

--- ocl/oclapi/management/data_integrity_checks.py
+++ ocl/oclapi/management/data_integrity_checks.py
@@ -3,7 +3,7 @@ import traceback
 def update_concepts_and_mappings_count(logger):
     from collection.models import CollectionVersion
     from sources.models import SourceVersion
-    logger.info('Updating concepts and mappings count and last updates on SourceVersions and CollectionVersions...')
+    logger.info('Updating concepts/mappings count and last updates on SourceVersions and CollectionVersions...')
     for source_version in SourceVersion.objects.all():
         try:
             source_version.save()
@@ -15,10 +15,24 @@ def update_concepts_and_mappings_count(logger):
         except Exception:
             logger.error(
                 'Failed to update CollectionVersion(%s) due to %s' % (collection_version.id, traceback.format_exc()))
+    logger.info('Done updating concepts/mappings count and last updates on SourceVersions and CollectionVersions...')
 
+def check_for_broken_references_in_collection_versions(logger):
+    logger.info('Checking for broken concept and mapping references in collection versions')
+    from collection.models import CollectionVersion
+    for collection_version in CollectionVersion.objects.all():
+        existing_concept_ids = list(collection_version.get_concepts().values_list('id', flat=True))
+        from collection.models import CollectionConcept
+        broken_concepts = CollectionConcept.objects.filter(collection_id=collection_version.id).exclude(concept_id__in=existing_concept_ids)
+        if broken_concepts:
+            logger.error('Removing %s broken concept references from collection version %s' %(broken_concepts.count(), collection_version.url))
+            broken_concepts.delete()
 
-
-
-
-
+        existing_mapping_ids = list(collection_version.get_mappings().values_list('id', flat=True))
+        from collection.models import CollectionMapping
+        broken_mappings = CollectionMapping.objects.filter(collection_id=collection_version.id).exclude(mapping_id__in=existing_mapping_ids)
+        if broken_mappings:
+            logger.error('Removing %s broken mapping references from collection version %s' %(broken_mappings.count(), collection_version.url))
+            broken_mappings.delete()
+    logger.info('Done checking for broken concept and mapping references in collection versions')
 
--- ocl/tasks.py
+++ ocl/tasks.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 
 import os
 
-from oclapi.management.data_integrity_checks import update_concepts_and_mappings_count
+from oclapi.management.data_integrity_checks import update_concepts_and_mappings_count, \
+    check_for_broken_references_in_collection_versions
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oclapi.settings.local')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Local')
@@ -29,7 +30,7 @@ celery.conf.ONCE_REDIS_URL = celery.conf.CELERY_RESULT_BACKEND
 
 @celery.task(base=QueueOnce, bind=True)
 def data_integrity_checks(self):
-    logger.info('Updating concepts and mappings count...')
+    check_for_broken_references_in_collection_versions(logger)
     update_concepts_and_mappings_count(logger)
 
 @celery.task(base=QueueOnce, bind=True)
