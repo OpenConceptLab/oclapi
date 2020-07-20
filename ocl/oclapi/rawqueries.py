93e6de3b79c6ecba9d9117b4a5b789ab24b2858a
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2020-07-20 11:40:05 +0200

    OpenConceptLab/ocl_issues#243 Properly delete ConceptVersion and MappingVersion with a missing SourceVersion

--- ocl/oclapi/rawqueries.py
+++ ocl/oclapi/rawqueries.py
@@ -51,44 +51,40 @@ class RawQueries():
 
     def delete_source(self, source):
         from sources.models import SourceVersion
-
         source_version_ids = list(SourceVersion.objects.filter(versioned_object_id=source.id).values_list('id', flat=True))
-
-        from mappings.models import MappingVersion
-        mapping_version_ids = list(MappingVersion.objects.filter(source_version_ids__in=source_version_ids).values_list('id', flat=True))
-        mapping_versions_col = self.db.get_collection('mappings_mappingversion')
-        mapping_versions_col.remove({'_id': {'$in': [ObjectId(id) for id in mapping_version_ids]}})
-        for mapping_version_id in mapping_version_ids:
-            remove_from_search_index(MappingVersion, mapping_version_id)
 
         from mappings.models import Mapping
         mapping_ids = list(Mapping.objects.filter(parent_id=source.id).values_list('id', flat=True))
-        mappings_col = self.db.get_collection('mappings_mapping')
-        mappings_col.remove({ 'parent_id': ObjectId(source.id)})
+
+        #Note that some MappingVersions may have been detached from all SourceVersions thus using versioned_object_id to delete
+        from mappings.models import MappingVersion
+        mapping_version_ids = list(MappingVersion.objects.filter(versioned_object_id__in=mapping_ids).values_list('id', flat=True))
+        MappingVersion.objects.filter(versioned_object_id__in=mapping_ids).delete()
+        for mapping_version_id in mapping_version_ids:
+            remove_from_search_index(MappingVersion, mapping_version_id)
+
+        Mapping.objects.filter(parent_id=source.id).delete()
         for mapping_id in mapping_ids:
             remove_from_search_index(Mapping, mapping_id)
 
+        from concepts.models import Concept
+        concept_ids = list(Concept.objects.filter(parent_id=source.id).values_list('id', flat=True))
+
+        #Note that some ConceptVersions may have been detached from all SourceVersions thus using versioned_object_id to delete
         from concepts.models import ConceptVersion
-        concept_version_ids = list(ConceptVersion.objects.filter(source_version_ids__in=source_version_ids).values_list('id', flat=True))
-        concept_versions_col = self.db.get_collection('concepts_conceptversion')
-        concept_versions_col.remove({'_id': {'$in': [ObjectId(id) for id in concept_version_ids]}})
+        concept_version_ids = list(ConceptVersion.objects.filter(versioned_object_id__in=concept_ids).values_list('id', flat=True))
+        ConceptVersion.objects.filter(versioned_object_id__in=concept_ids).delete()
         for concept_version_id in concept_version_ids:
             remove_from_search_index(ConceptVersion, concept_version_id)
 
-        from concepts.models import Concept
-        concept_ids = list(Concept.objects.filter(parent_id=source.id).values_list('id', flat=True))
-        concepts_col = self.db.get_collection('concepts_concept')
-        concepts_col.remove({'parent_id': source.id})
+        Concept.objects.filter(parent_id=source.id).delete()
         for concept_id in concept_ids:
             remove_from_search_index(Concept, concept_id)
 
-        source_versions_col = self.db.get_collection('sources_sourceversion')
-        source_versions_col.remove({'versioned_object_id': source.id})
+        SourceVersion.objects.filter(versioned_object_id=source.id).delete()
         for source_version_id in source_version_ids:
             remove_from_search_index(SourceVersion, source_version_id)
 
-        sources_col = self.db.get_collection('sources_source')
-        sources_col.remove({'_id': ObjectId(source.id)})
-
         from sources.models import Source
+        Source.objects.filter(id=source.id).delete()
         remove_from_search_index(Source, source.id)
